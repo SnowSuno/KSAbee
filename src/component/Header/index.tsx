a59e937b99e40dc595f29ceeb4bfea68cc3548ed
jiwon79 <19-079@ksa.hs.kr>
2021-11-13 01:52:39 +0900

    feat: Add header styles

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import "./style.css"
 import logo from "./../../img/logo.png"
 
 function Header() {
   return (
-  <div>
-    <img src={logo} alt="logo img" />
-    KSABee
-    KSA LOL랭킹 사이트
+  <div className="header">
+    <img src={logo} alt="logo img" className="header__img" />
+    <div className="header__text">
+      <span>KSA</span>
+      <span>Bee</span><br/>
+      <span>KSA LOL랭킹 사이트</span>
+    </div>
   </div>
   );
 }
