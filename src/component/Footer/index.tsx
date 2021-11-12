664488b050be51341ae2328b694c5085e95f3b37
jiwon79 <19-079@ksa.hs.kr>
2021-11-13 02:28:51 +0900

    feat: Add footer

---
+++ src/component/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>KSA League of Legend 전적검색 WEBSITE</p>
+      <p>제작자 : 권순호, 이지원</p>
+      <p>&copy; 2021. 권순호, 이지원 all rights reserved.</p>
+      <p>CONTACT US: 19-006@ksa.hs.kr, 19-079@ksa.hs.kr</p>
+    </div>
+  )
+}
+
+export default Footer;
