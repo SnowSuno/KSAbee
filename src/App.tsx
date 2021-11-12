2391a29c74afac18b5d1e6319bf47ba7b226e73a
jiwon79 <19-079@ksa.hs.kr>
2021-11-13 02:27:15 +0900

    refactor: Devide App.tsx to view and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
-import React, {useEffect, useState} from "react";
-import { Account } from "./common/api";
-import { AccountType } from "./common/types";
+import React from "react";
 
 import Header from "./component/Header";
 import Toolbar from "./component/Toolbar";
 import ProfileList from "./component/ProfileList";
-
+import Fotter from "./component/Footer"
 
 export default function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [accountList, setAccountList] = useState<AccountType[]>([]);
-  console.log(loading, accountList);
-  // const [sort, setSort] = useState({
-  //     key: 'tier',
-  //     reverse: false,
-  // });
-
-  useEffect(() => {
-    async function fetchApi() {
-      try {
-        const userAccountList = await Account.getAccounts();
-        setAccountList(userAccountList);
-      } catch (e){
-        console.log(e)
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchApi();
-  }, [])
-
   return (
     <div>
       <Header />
       <Toolbar />
       <ProfileList />
+      <Fotter />
     </div>
   );
 }
--- src/component/ProfileList/index.tsx
+++ src/component/ProfileList/index.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import { AccountType } from "../../common/types";
+import { Account } from "../../common/api";
+
 function ProfileList() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [accountList, setAccountList] = useState<AccountType[]>([]);
+  console.log(loading, accountList);
+  // const [sort, setSort] = useState({
+  //     key: 'tier',
+  //     reverse: false,
+  // });
 
-    return (
-        <div>
+  useEffect(() => {
+    async function fetchApi() {
+      try {
+        const userAccountList = await Account.getAccounts();
+        setAccountList(userAccountList);
+      } catch (e){
+        console.log(e)
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchApi();
+  }, [])
 
-        </div>
-    );
+  return (
+    <div>
+      ddd
+    </div>
+  );
 }
 
 export default ProfileList;
