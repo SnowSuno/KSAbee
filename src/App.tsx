e5e1febc4cf37fcbddf9452ceaafa616a603be21
jiwon79 <19-079@ksa.hs.kr>
2021-11-17 10:10:25 +0900

    refactor: Move thead, tbody to parents component

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 import Header from "./component/Header";
 import Toolbar from "./component/Toolbar";
-import ProfileList from "./component/ProfileTable";
-import Fotter from "./component/Footer"
+import ProfileTable from "./component/ProfileTable";
+import Footer from "./component/Footer"
 
 export default function App() {
   return (
     <div>
       <Header />
       <Toolbar />
-      <ProfileList />
-      <Fotter />
+      <ProfileTable />
+      <Footer />
     </div>
   );
 }
--- src/component/ProfileTable/ProfileTableHead/index.tsx
+++ src/component/ProfileTable/ProfileTableHead/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import ProfileItem from "../../ProfileItem";
 
 const ProfileTableHead = () => {
   return (
-    <thead>
-      <tr>
-        <th>순위</th>
-        <th>프로필</th>
-        <th>티어</th>
-        <th>레벨</th>
-        <th>승률</th>
-        <th>포지션</th>
-      </tr>
-    </thead>
+    <tr>
+      <th>순위</th>
+      <th>프로필</th>
+      <th>티어</th>
+      <th>레벨</th>
+      <th>승률</th>
+      <th>포지션</th>
+    </tr>
   )
 }
 
