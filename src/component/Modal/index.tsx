dae1956d4fe1c1de5db24f6905b1dcf66efb868a
jiwon79 <19-079@ksa.hs.kr>
2021-12-15 02:10:33 +0900

    style: Position loader and modal on center

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -13,34 +13,36 @@ interface ModalProps {
 const Modal = ({showModal, handleShowModal, fetchUserAccounts}: ModalProps) => {
   return (
       <div className={showModal === 'null' ? '' : 'background'}>
-        {showModal === 'load'
-          ? <div className="loader">
-          </div>
-          : ''
-        }
-        <div className="modal">
-          {showModal === 'load' || showModal === 'null' ? '' :
-            <div>
-              <button onClick={() => handleShowModal('null')}>x</button>
+        <div className="center">
+          {showModal === 'load'
+            ? <div className="loader">
             </div>
-          }
-          {showModal === 'register'
-            ? <RegisterModal
-              handleShowModal={handleShowModal}
-              fetchUserAccounts={fetchUserAccounts}
-            />
-            : showModal === 'update'
-            ? <UpdateModal
-                handleShowModal={handleShowModal}
-                fetchUserAccounts={fetchUserAccounts}
-              />
-            : showModal === 'delete'
-            ? <DeleteModal
-                handleShowModal={handleShowModal}
-                fetchUserAccounts={fetchUserAccounts}
-              />
             : ''
           }
+          <div className="modal">
+            {showModal === 'load' || showModal === 'null' ? '' :
+              <div>
+                <button onClick={() => handleShowModal('null')}>x</button>
+              </div>
+            }
+            {showModal === 'register'
+              ? <RegisterModal
+                handleShowModal={handleShowModal}
+                fetchUserAccounts={fetchUserAccounts}
+              />
+              : showModal === 'update'
+              ? <UpdateModal
+                  handleShowModal={handleShowModal}
+                  fetchUserAccounts={fetchUserAccounts}
+                />
+              : showModal === 'delete'
+              ? <DeleteModal
+                  handleShowModal={handleShowModal}
+                  fetchUserAccounts={fetchUserAccounts}
+                />
+              : ''
+            }
+          </div>
         </div>
       </div>
   )
