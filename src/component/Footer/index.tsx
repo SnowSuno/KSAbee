1b02f1f1d56eb272393c6c6990fb0d4ff6da3ffa
SnowSuno <snowsuno.dev@gmail.com>
2022-02-05 19:46:49 +0900

    feat : footer styling

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -5,7 +5,6 @@ function Footer() {
   return (
     <div className="footer">
       <p>KSA 리그 오브 레전드 전적검색 사이트</p>
-      {/*<p>제작자 : 권순호, 이지원</p>*/}
       <p>&copy; 2021. 권순호, 이지원 All rights reserved.</p>
       <p>CONTACT US: 19-006@ksa.hs.kr, 19-079@ksa.hs.kr</p>
     </div>
